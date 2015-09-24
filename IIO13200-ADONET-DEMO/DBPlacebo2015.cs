54fdd1d4545467f6271f06f56520d2a118afc600
Juuso Ohra-aho <Juuso.ohra-aho@protonmail.com>
2015-09-24 17:25:41 +0300

    T6 palautus

---
+++ IIO13200-ADONET-DEMO/DBPlacebo2015.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.ICT.JSON
+{
+    public class JSONPlacebo2015
+    {
+        public bool isInitialized;
+        public JSONPlacebo2015()
+        {
+            isInitialized = true;
+        }
+        public string Yield()
+        {
+            //TODO hae opettajan kertomasta palvelusta JSONia
+            return "Hello JSON!";
+        }
+
+        public static DataTable GetTestCustomers()
+        {
+            //create
+            DataTable dt = new DataTable();
+            //columns
+            dt.Columns.Add("ID", typeof(string));
+            dt.Columns.Add("LastName", typeof(string));
+            dt.Columns.Add("FirstName", typeof(string));
+            dt.Columns.Add("Address", typeof(string));
+            dt.Columns.Add("ZIP", typeof(string));
+            dt.Columns.Add("City", typeof(string));
+            //rows
+            dt.Rows.Add("A3581", "Waltari", "Mika", "Piippukatu 2", "42052", "Jyväskylä");
+            dt.Rows.Add("B3553", "King", "Stephen", "Piippukatu 2", "42052", "Jyväskylä");
+            dt.Rows.Add("C1238", "Neruda", "Pablo", "Piippukatu 2", "42052", "Jyväskylä");
+            dt.Rows.Add("D9876", "Oksanen", "Sofi", "Piippukatu 2", "42052", "Jyväskylä");
+            return dt;
+        }
+
+        public static DataTable GetAllCustomersFromSQLServer(string city, string connectionStr, string taulu, out string viesti)
+        {
+            // basic principle: connect - execute query - disconnect
+            try
+            {
+                SqlConnection myConn = new SqlConnection(connectionStr);
+                myConn.Open();
+                SqlCommand cmd;
+                if (city == "")
+                {
+                    cmd = new SqlCommand("SELECT * FROM " + taulu, myConn);
+                } else
+                {
+                    cmd = new SqlCommand("SELECT * FROM " + taulu + " WHERE city='" + city +"'", myConn);
+                }
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds, taulu);
+                viesti = "Tiedot haettu onnistuneesti tietokannasta " + myConn.DataSource;
+                return ds.Tables[taulu];
+            }
+            catch (Exception ex)
+            {
+                viesti = ex.Message;
+                throw;
+            }
+        }
+
+
+    }
+}
---
+++ IIO13200-ADONET-DEMO/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿//Koodannut ja testannut toimivaksi 6.3.2014 EsaSalmik
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JAMK.ICT.ADOBlanco
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+        string currentCity = "";
+
+    public MainWindow()
+    {
+      InitializeComponent();
+      IniMyStuff();
+    }
+
+    private void IniMyStuff()
+    {
+      //TODO täytetään combobox asiakkaitten maitten nimillä
+      //esimerkki kuinka App.Configissa oleva connectionstring luetaan
+      lbMessages.Content = JAMK.ICT.Properties.Settings.Default.Tietokanta;
+
+            cbCountries.Items.Add("Kaikki");
+            string successViesti, city = "";
+            DataTable dt = JAMK.ICT.JSON.JSONPlacebo2015.GetAllCustomersFromSQLServer(city, JAMK.ICT.Properties.Settings.Default.Tietokanta, JAMK.ICT.Properties.Settings.Default.Taulu, out successViesti);
+            foreach (DataRow dr in dt.Rows)
+            {
+                if (!cbCountries.Items.Contains(dr[5].ToString())) {
+                    cbCountries.Items.Add(dr[5]);
+                }
+            }
+            cbCountries.SelectedItem = "Kaikki";
+        }
+
+    private void btnGet3_Click(object sender, RoutedEventArgs e)
+    {
+        //TODO TEST
+        dgCustomers.DataContext = JAMK.ICT.JSON.JSONPlacebo2015.GetTestCustomers();
+    }
+
+    private void btnGetAll_Click(object sender, RoutedEventArgs e)
+    {
+        //TODO GET ALL
+        string successViesti, city = "";
+        dgCustomers.DataContext = JAMK.ICT.JSON.JSONPlacebo2015.GetAllCustomersFromSQLServer(city, JAMK.ICT.Properties.Settings.Default.Tietokanta, JAMK.ICT.Properties.Settings.Default.Taulu, out successViesti);
+        lbMessages.Content = successViesti;
+    }
+
+    private void btnGetFrom_Click(object sender, RoutedEventArgs e)
+    {
+            //TODO GET FROM
+            if(currentCity == "Kaikki")
+            {
+                currentCity = "";
+            }
+            string successViesti;
+            dgCustomers.DataContext = JAMK.ICT.JSON.JSONPlacebo2015.GetAllCustomersFromSQLServer(currentCity, JAMK.ICT.Properties.Settings.Default.Tietokanta, JAMK.ICT.Properties.Settings.Default.Taulu, out successViesti);
+            lbMessages.Content = successViesti;
+            btnGetFrom.IsEnabled = false;
+        }
+
+        private void btnYield_Click(object sender, RoutedEventArgs e)
+        {
+            JAMK.ICT.JSON.JSONPlacebo2015 roskaa = new JAMK.ICT.JSON.JSONPlacebo2015();
+            MessageBox.Show(roskaa.Yield());
+        }
+
+        private void cbCountries_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            currentCity = cbCountries.SelectedItem.ToString();
+            btnGetFrom.IsEnabled = true;
+        }
+    }
+}
